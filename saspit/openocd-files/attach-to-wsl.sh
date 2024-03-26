8abe0ee70da8622bb449ff5023c95266aa435a5e
Philipp Fuchs <philipp.fuchs@ingenics-digital.com>
2024-03-26 17:24:22 +0100

    Adapt openocd utilities to usbipd usage changes

--- saspit/openocd-files/attach-to-wsl.sh
+++ saspit/openocd-files/attach-to-wsl.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-RESULT=$(powershell.exe -Command "usbipd wsl list" | grep 0403:6010 | cut -d ' ' -f1)
+RESULT=$(powershell.exe -Command "usbipd list" | grep 0403:6010 | cut -d ' ' -f1)
 
 if [ ! -z "$RESULT" ]
 then
-	powershell.exe -Command "usbipd wsl attach --busid $RESULT"
+	powershell.exe -Command "usbipd attach --wsl --busid $RESULT"
 	echo "Attached device with busid $RESULT"
 else
 	echo "Device could not be attached to WSL :("
--- saspit/openocd-files/detach-from-wsl.sh
+++ saspit/openocd-files/detach-from-wsl.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-RESULT=$(powershell.exe -Command "usbipd wsl list" | grep 0403:6010 | cut -d ' ' -f1)
+RESULT=$(powershell.exe -Command "usbipd list" | grep 0403:6010 | cut -d ' ' -f1)
 
 if [ ! -z "$RESULT" ]
 then
-	powershell.exe -Command "usbipd wsl detach --busid $RESULT"
+	powershell.exe -Command "usbipd detach --busid $RESULT"
 	echo "Detached device with busid $RESULT"
 else
 	echo "Device could not be detached from WSL :("
